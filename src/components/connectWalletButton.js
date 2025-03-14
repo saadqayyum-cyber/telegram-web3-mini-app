d3078fb04a9488ed4fba20a0f4e42f1b51f88e13
SAAD-PC\saadq <saadqayyum794@gmail.com>
2025-03-14 23:20:15 +0500

    weird

--- src/components/connectWalletButton.js
+++ src/components/connectWalletButton.js
@@ -4,6 +4,12 @@ import ConnectedButton from "./connectedButton";
 import SwitchChain from "./switchChain";
 import { ConnectButton } from "thirdweb/react";
 
+import { createThirdwebClient } from "thirdweb";
+
+const client = createThirdwebClient({
+  clientId: "67f33a7eb74f521dc14626f8f2b8ea5b",
+});
+
 const ConnectWalletButton = ({ token0, token1, amount, chain, status }) => {
   const currentChain = useChain(); //thirweb hook to get the current chainof the wallet in session
 
@@ -11,9 +17,16 @@ const ConnectWalletButton = ({ token0, token1, amount, chain, status }) => {
     if (!chain) {
       console.log("chain not selected");
       if (status === "disconnected") {
-        return <ConnectButton />;
+        return (
+          <ConnectButton
+            client={client}
+            walletConnect={{
+              projectId: "864eb069031c6a2e6b20f5f8707c0258",
+            }}
+          />
+        );
       }
-     
+
       if (status === "connected") {
         return <ConnectedButton />;
       }
@@ -22,7 +35,7 @@ const ConnectWalletButton = ({ token0, token1, amount, chain, status }) => {
       if (status === "connected" && chain.chainId !== currentChain.chainId + "") {
         return <SwitchChain chain={chain} />;
       }
-     
+
       if (status === "connected" && chain.chainId === currentChain.chainId + "") {
         return <ConnectedButton />;
       }
